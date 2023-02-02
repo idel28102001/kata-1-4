df106ee7f943e89793421cb631e60ba61f7e5fc1
Idel Gataullin <idel28102001@mail.ru>
2023-02-02 13:47:27 +0300

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# kata-1-3
+
+https://idel28102001.github.io/kata-1-3/
---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+// const element = document.querySelector('.header');
+// const button = document.querySelector('.header__burger');
+// button.addEventListener('click', ()=>{
+//     element.classList.toggle('mobile-menu--active');
+// });
